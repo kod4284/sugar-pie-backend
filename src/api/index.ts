fadb89b54434b4a28916076e9cad84d89ee3e1ed
Daewoong <kodw4284@gmail.com>
2020-03-27 22:20:47 +0900

    Add posts endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import Router from 'koa-router';
+import posts from '@/posts';
+
 const api = new Router();
 
-api.get('/test', ctx => {
-  ctx.body = 'test success';
-});
+api.use('/posts', posts.routes());
 
 export default api;
---
+++ src/posts/index.ts
@@ -0,0 +1,19 @@
+import Router from 'koa-router';
+const posts = new Router();
+
+const printInfo = (ctx: any) => {
+  ctx.body = {
+    method: ctx.method,
+    path: ctx.path,
+    params: ctx.params,
+  };
+};
+
+posts.get('/', printInfo);
+posts.post('/', printInfo);
+posts.get('/:id', printInfo);
+posts.delete('/:id', printInfo);
+posts.put('/:id', printInfo);
+posts.patch('/:id', printInfo);
+
+export default posts
