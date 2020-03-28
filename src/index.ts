afd94ba7b4c010923866db65d6b33acb986c7a7d
Daewoong <kodw4284@gmail.com>
2020-03-28 23:24:49 +0900

    Connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,26 @@ import 'module-alias/register';
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
-import 'dotenv';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 import api from '@/api';
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI || 'mongodb://localhost:27017/default', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connected to MongoDB');
+  })
+  .catch((e: Error) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -12,11 +29,9 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-const port = process.env.PORT || 4000;
+const port = PORT || 4000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Listening to port %d', port);
