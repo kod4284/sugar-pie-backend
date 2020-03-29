d4b5c34552bd92bba9982eb4f2a68896c9880d98
Daewoong <kodw4284@gmail.com>
2020-03-29 15:59:44 +0900

    Refactor posts.ctrl.ts

--- src/api/posts/posts.ctrl.ts
+++ src/api/posts/posts.ctrl.ts
@@ -1,85 +1,66 @@
 import { Context as _ctx } from 'koa';
+import Post from '@/model/post';
 
-let postId = 1;
-
-const posts = [
-  {
-    id: 1,
-    title: 'Title',
-    body: 'Contents',
-  },
-];
-
-export const write = (ctx: _ctx) => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
-
-export const list = (ctx: _ctx) => {
-  ctx.body = posts;
-};
-
-export const read = (ctx: _ctx) => {
-  const { id } = ctx.params;
-  const post = posts.find(p => p.id.toString() === id);
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'No post exists.',
-    };
-    return;
+export const write = async (ctx: _ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
-export const remove = (ctx: _ctx) => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'No post exists.',
-    };
-    return;
+export const list = async (ctx: _ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts.splice(index, 1);
-  ctx.status = 204;
 };
 
-export const replace = (ctx: _ctx) => {
+export const read = async (ctx: _ctx) => {
   const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'No post exists.',
-    };
-    return;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
 
-export const update = (ctx: _ctx) => {
+export const remove = async (ctx: _ctx) => {
   const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'No post exists',
-    };
-    return;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
+};
+
+export const update = async (ctx: _ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
