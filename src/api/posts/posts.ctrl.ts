d53ceb69156dae1f1b24a8782c039d07dedcaf44
Daewoong <kodw4284@gmail.com>
2020-03-29 16:47:36 +0900

    Validate post methods

--- src/api/posts/posts.ctrl.ts
+++ src/api/posts/posts.ctrl.ts
@@ -1,6 +1,7 @@
 import { Context as _ctx } from 'koa';
 import Post from '@/model/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -13,20 +14,6 @@ export const checkObjectId = (ctx: _ctx, next: Function) => {
   return next();
 };
 
-export const write = async (ctx: _ctx) => {
-  const { title, body, tags } = ctx.request.body;
-  const post = new Post({
-    title,
-    body,
-    tags,
-  });
-  try {
-    await post.save();
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
 
 export const list = async (ctx: _ctx) => {
   try {
@@ -61,8 +48,51 @@ export const remove = async (ctx: _ctx) => {
   }
 };
 
+export const write = async (ctx: _ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
 export const update = async (ctx: _ctx) => {
   const { id } = ctx.params;
+
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true,
