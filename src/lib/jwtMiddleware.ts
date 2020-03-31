ca6ccb24ffc71b60be482ee92fcb252139679a75
Daewoong <kodw4284@gmail.com>
2020-03-31 23:51:44 +0900

    Add a function to reissue token before it is expired

--- src/lib/jwtMiddleware.ts
+++ src/lib/jwtMiddleware.ts
@@ -1,14 +1,34 @@
 import jwt from 'jsonwebtoken';
 import { Context as _ctx } from 'koa';
+import User from '@/model/user';
 
 interface UserInfo {
-  _id: string,
-  username: string | object
+  _id: string;
+  username: string | object;
+  iat: number;
+  exp: number;
 }
 
-const jwtMiddleware = (ctx: _ctx, next: Function) => {
+const reissueToken = async (ctx: _ctx, next: Function, decoded: UserInfo) => {
+  const now = Math.floor(Date.now() / 1000);
+  if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+    console.log('d');
+    const user = await User.findById(decoded._id);
+    if (user === null) {
+      throw new Error('Cannot find user information');
+    }
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
+  }
+};
+
+const jwtMiddleware = async (ctx: _ctx, next: Function) => {
   const token = ctx.cookies.get('access_token');
   if (!token) {
+    console.log('pass');
     return next();
   }
   try {
@@ -16,14 +36,16 @@ const jwtMiddleware = (ctx: _ctx, next: Function) => {
     if (!secret) {
       throw new Error('JWT_SECRET does not exist');
     }
-    const decoded = jwt.verify(token, secret);
-    if (typeof decoded === 'string') return;
 
-    const { _id, username } = decoded as UserInfo;
+    const decoded = jwt.verify(token, secret) as UserInfo;
+    if (typeof decoded === 'string') return;
+    const { _id, username } = decoded;
     ctx.state.user = {
       _id,
       username,
     };
+
+    await reissueToken(ctx, next, decoded);
     console.log(decoded);
     return next();
   } catch (e) {
